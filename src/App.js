4bb7ccfa595e1db41a01fb385067964c7707dfe7
Victor Escalona <victorescalonag@gmail.com>
2021-03-01 03:58:51 -0500

    Added a new route, when the route doesn't exists it will redirect to Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Dashboard from "./pages/User/Dashboard/Dashboard";
 
 function App() {
   return (
     <>
       <Switch>
-        <Route exact path="" component={Login} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="*" component={Login} />
       </Switch>
     </>
   );
